050edf6d23ec5d695239fd52fb28189bf45f3a20
user <user@cu-cs-vm.(none)>
2014-03-28 00:46:50 -0600

    Fork Added to pi-sched

--- pi-sched.c
+++ pi-sched.c
@@ -9,13 +9,17 @@
  *      calculating pi using a specific scheduling policy.
  */
 
+//Referenced: stack overflow 19099663/how-to-correctly-use-fork-exec-wait
+
 /* Local Includes */
 #include <stdlib.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
 #include <errno.h>
 #include <sched.h>
+#include <sys/wait.h>
 
 #define DEFAULT_ITERATIONS 1000000
 #define RADIUS (RAND_MAX / 2)
@@ -39,6 +43,10 @@ int main(int argc, char* argv[]){
     double inSquare = 0.0;
     double pCircle = 0.0;
     double piCalc = 0.0;
+    int processes;
+    int p = 0;
+    pid_t pid;
+    pid_t parent = getpid();
 
     /* Process program arguments to select iterations and policy */
     /* Set default iterations if not supplied */
@@ -72,8 +80,8 @@ int main(int argc, char* argv[]){
 	    fprintf(stderr, "Unhandeled scheduling policy\n");
 	    exit(EXIT_FAILURE);
 	}
-    }
-    
+    } 
+   
     /* Set process to max prioty for given scheduler */
     param.sched_priority = sched_get_priority_max(policy);
     
@@ -86,22 +94,50 @@ int main(int argc, char* argv[]){
     }
     fprintf(stdout, "New Scheduling Policy: %d\n", sched_getscheduler(0));
 
-    /* Calculate pi using statistical methode across all iterations*/
-    for(i=0; i<iterations; i++){
-	x = (random() % (RADIUS * 2)) - RADIUS;
-	y = (random() % (RADIUS * 2)) - RADIUS;
-	if(zeroDist(x,y) < RADIUS){
-	    inCircle++;
-	}
-	inSquare++;
-    }
+    //Enable Forking:
+    if(argv[3]) processes = atoi(argv[3]);
+    else processes = 1;
+    
+    fprintf(stdout, "My Proccess Count is: %d\n", processes);
 
-    /* Finish calculation */
-    pCircle = inCircle/inSquare;
-    piCalc = pCircle * 4.0;
+    for(p = 0; p < processes; p++) 
+    { 
+		pid = fork();
 
-    /* Print result */
-    fprintf(stdout, "pi = %f\n", piCalc);
+        if (pid == -1) return 1;
+
+        else if(pid == 0)
+        {
+			
+	        //fprintf(stdout, "I am pid: %d\n", pid);
+	    
+            /* Calculate pi using statistical methode across all iterations*/
+            for(i=0; i<iterations; i++){
+	            x = (random() % (RADIUS * 2)) - RADIUS;
+	            y = (random() % (RADIUS * 2)) - RADIUS;
+	        if(zeroDist(x,y) < RADIUS){
+	        inCircle++;
+	                }
+	    inSquare++;
+            }
+
+        /* Finish calculation */
+        pCircle = inCircle/inSquare;
+        piCalc = pCircle * 4.0;
+
+        /* Print result */
+        fprintf(stdout, "pi = %f\n", piCalc);
+        break;
+        }
+        
+        else if(pid > 1)
+        {
+	        //fprintf(stdout, "I should be parent with pid: %d\n", pid);
+            int status;
+            waitpid(pid, &status, 0);
+        }
+
+}
 
     return 0;
 }
